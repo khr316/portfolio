1e8bbfbff0727f8594cd51e46022e99acdab4707
khr316 <xhfhxhsxhs@gmail.com>
2024-10-08 09:32:07 +0900

    khr316

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,17 @@
-// 스크롤 시 버튼 표시
-window.onscroll = function() {
-    const scrollTopBtn = document.getElementById('scrollTopBtn');
+// 스크롤 버튼 처리
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// 버튼 표시 및 숨기기
+window.onscroll = function () {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollTopBtn.style.display = 'block';
+        scrollTopBtn.style.display = "block";
     } else {
-        scrollTopBtn.style.display = 'none';
+        scrollTopBtn.style.display = "none";
     }
 };
 
-// 맨 위로 가기 버튼 클릭 시 스크롤
-document.getElementById('scrollTopBtn').addEventListener('click', function() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-});
+// 버튼 클릭 시 맨 위로 스크롤
+scrollTopBtn.onclick = function () {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+};
